fbfae7e88342b112139b61a59ff0a1cf35da562b
rusins <raitis.krikis@gmail.com>
2017-12-18 14:19:03 +0000

    Header Bar Album art now fits to scale

--- src/boomlin_app_window.cpp
+++ src/boomlin_app_window.cpp
@@ -6,18 +6,32 @@ BoomlinAppWindow::BoomlinAppWindow(BaseObjectType* cobject,
 								   const Glib::RefPtr<Gtk::Builder>& ref_builder) : Gtk::ApplicationWindow(cobject),
   m_refBuilder(ref_builder) {
   
-}
+  }
 
 //static
 BoomlinAppWindow* BoomlinAppWindow::create() {
   // Load the Builder file and instantiate its widgets.
   auto ref_builder = Gtk::Builder::create_from_resource("/com/github/rusins/boomlin/window.ui");
 
-  BoomlinAppWindow* window = nullptr;
+  BoomlinAppWindow *window = nullptr;
   ref_builder->get_widget_derived(WINDOW_WIDGET_NAME, window);
   if (!window)
 	throw std::runtime_error("No " + WINDOW_WIDGET_NAME + " object in window.ui");
 
+  // Temp code to make window pretty
+  Gtk::Image *image = nullptr;
+  ref_builder->get_widget("header_bar_album_art_image", image);
+  if (!image)
+	throw std::runtime_error("No album art image found!");
+
+  auto pixbuf = Gdk::Pixbuf::create_from_file("/home/raitis/Music/WRLD/Awake EP/cover.jpg");
+  auto scaled = pixbuf->scale_simple(48, 48, Gdk::INTERP_BILINEAR);
+  image->set(scaled);
+  
+  
+  
+  // End of temp code
+  
   return window;
 }
 
